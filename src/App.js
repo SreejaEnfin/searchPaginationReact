d015c3a4359a368d213977fbf34313fdd61c9ab3
SreejaEnfin <sreeja.pm@enfintechnologies.com>
2023-10-19 12:40:59 +0530

    basic table using Tanstack React table

--- src/App.js
+++ src/App.js
@@ -1,93 +1,80 @@
 import React, { useState } from "react";
 import "./App.css";
-import data from "./data.json"; // Assuming you have JSON data with a structure similar to your previous messages
+import mdata from "./data.json";
+import {
+  useReactTable,
+  getCoreRowModel,
+  flexRender,
+} from "@tanstack/react-table";
 
 function App() {
-  const [searchValue, setSearchValue] = useState("");
-  const [results, setResults] = useState(data.results.slice(0, 49));
+  // using tanstack
+  const [data, setResults] = useState(mdata.results.slice(0, 49));
+  // const data = useMemo(() => results, []);
 
-  const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 10;
-  const startPoint = (currentPage - 1) * itemsPerPage;
-  const endPoint = startPoint + itemsPerPage;
+  /** @type import('@tanstack/react-table').columnDef<any>*/
+  const columns = [
+    {
+      header: "Gender",
+      accessorKey: "gender",
+    },
+    {
+      header: "Name",
+      accessorKey: "name.first",
+    },
+    {
+      header: "Location",
+      accessorKey: "location.country",
+    },
+    {
+      header: "Email",
+      accessorKey: "email",
+    },
+    {
+      header: "Login",
+      accessorKey: "login.username",
+    },
+  ];
 
-  const onSearch = (e) => {
-    setSearchValue(e.target.value);
-  };
+  console.log(columns);
 
-  const filteredData = searchValue
-    ? results.filter((item) => {
-        const search = searchValue.toLowerCase();
-        return (
-          item.name.first.toLowerCase().includes(search) ||
-          item.email.toLowerCase().includes(search) ||
-          item.location.country.toLowerCase().includes(search) ||
-          item.gender.toLowerCase().includes(search) ||
-          item.login.username.toLowerCase().includes(search)
-        );
-      })
-    : results;
-
-  const currentData = filteredData.slice(startPoint, endPoint);
-  // const totalPages = filteredData.length / itemsPerPage;
-  const totalPages = Math.ceil(filteredData.length / itemsPerPage);
-
-  const pageChange = (i) => {
-    console.log(filteredData.length);
-    console.log(totalPages);
-    if (i >= 1 && i <= totalPages) {
-      setCurrentPage(i);
-    }
-    // if (i !== 1 || i !== endPoint) {
-    //   setCurrentPage(i);
-    // }
-  };
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
 
   return (
-    <div className="App">
-      <input
-        type="text"
-        placeholder="Search by Name, Email, or Location"
-        value={searchValue}
-        onChange={onSearch}
-      />{" "}
-      &nbsp;
-      <table className="table">
+    <div>
+      <table>
         <thead>
-          <tr>
-            <th>Gender</th>
-            <th>Name</th>
-            <th>Location</th>
-            <th>Email</th>
-            <th>Login</th>
-          </tr>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <th key={header.id}>
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                </th>
+              ))}
+            </tr>
+          ))}
         </thead>
         <tbody>
-          {currentData.map((item, index) => (
-            <tr key={index}>
-              <td>{item.gender}</td>
-              <td>{item.name.first}</td>
-              <td>{item.location.country}</td>
-              <td>{item.email}</td>
-              <td>{item.login.username}</td>
+          {table.getRowModel().rows.map((row) => (
+            <tr key={row.id}>
+              {row.getVisibleCells().map((cell) => (
+                <td key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
             </tr>
           ))}
         </tbody>
       </table>
-      <div className="pagination">
-        <button
-          onClick={() => pageChange(currentPage - 1)}
-          disabled={currentPage === 1}
-        >
-          Previous
-        </button>
-        <button
-          onClick={() => pageChange(currentPage + 1)}
-          disabled={currentPage === totalPages}
-        >
-          Next
-        </button>
-      </div>
     </div>
   );
 }
